3606166c683bf82c88b01aae5780aa07947b187c
mummsiimiktkhk <siim.mumm@khk.ee>
2020-04-26 01:55:27 +0300

    modified file `config.php`

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:4:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1588460100;s:7:"authKey";s:64:"wc(coZ4e|w@4*S0ix*byvGrGBq$SS%lY[Rvh3n?FtP luFK{WoS7* :X%$CEsUhN";}
+a:5:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1588460100;s:7:"authKey";s:64:"wc(coZ4e|w@4*S0ix*byvGrGBq$SS%lY[Rvh3n?FtP luFK{WoS7* :X%$CEsUhN";s:7:"version";s:5:"1.0.4";}
