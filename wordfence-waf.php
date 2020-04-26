f21e5d2ec074bbd0ef2bf90ec1f126f349f1a300
mummsiimiktkhk <siim.mumm@khk.ee>
2020-04-26 15:47:16 +0300

    modified file `wordfence-waf.php`

---
+++ wordfence-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+if (file_exists('/home/mummsiimiktkhk/public_html/veebiarendus/wordpress/wordpress/wp-content/plugins/wordfence/waf/bootstrap.php')) {
+	define("WFWAF_LOG_PATH", '/home/mummsiimiktkhk/public_html/veebiarendus/wordpress/wordpress/wp-content/wflogs/');
+	include_once '/home/mummsiimiktkhk/public_html/veebiarendus/wordpress/wordpress/wp-content/plugins/wordfence/waf/bootstrap.php';
+}
+?>
